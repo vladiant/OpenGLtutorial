6abcc7af4559598e1d28a229867dc37a82e8d20c
Vladislav Antonov <vladislav.antonov@gmail.com>
2022-07-07 06:56:11 +0300

    Fix vertex initialization

--- 12_MeshClass/main.cpp
+++ 12_MeshClass/main.cpp
@@ -24,14 +24,14 @@ Vertex vertices[] = {  //               COORDINATES           / COLORS /
 GLuint indices[] = {0, 1, 2, 0, 2, 3};
 
 Vertex lightVertices[] = {  //     COORDINATES     //
-    Vertex{glm::vec3(-0.1f, -0.1f, 0.1f)},
-    Vertex{glm::vec3(-0.1f, -0.1f, -0.1f)},
-    Vertex{glm::vec3(0.1f, -0.1f, -0.1f)},
-    Vertex{glm::vec3(0.1f, -0.1f, 0.1f)},
-    Vertex{glm::vec3(-0.1f, 0.1f, 0.1f)},
-    Vertex{glm::vec3(-0.1f, 0.1f, -0.1f)},
-    Vertex{glm::vec3(0.1f, 0.1f, -0.1f)},
-    Vertex{glm::vec3(0.1f, 0.1f, 0.1f)}};
+    Vertex{glm::vec3(-0.1f, -0.1f, 0.1f), {}, {}, {}},
+    Vertex{glm::vec3(-0.1f, -0.1f, -0.1f), {}, {}, {}},
+    Vertex{glm::vec3(0.1f, -0.1f, -0.1f), {}, {}, {}},
+    Vertex{glm::vec3(0.1f, -0.1f, 0.1f), {}, {}, {}},
+    Vertex{glm::vec3(-0.1f, 0.1f, 0.1f), {}, {}, {}},
+    Vertex{glm::vec3(-0.1f, 0.1f, -0.1f), {}, {}, {}},
+    Vertex{glm::vec3(0.1f, 0.1f, -0.1f), {}, {}, {}},
+    Vertex{glm::vec3(0.1f, 0.1f, 0.1f), {}, {}, {}}};
 
 GLuint lightIndices[] = {0, 1, 2, 0, 2, 3, 0, 4, 7, 0, 7, 3, 3, 7, 6, 3, 6, 2,
                          2, 6, 5, 2, 5, 1, 1, 5, 4, 1, 4, 0, 4, 5, 6, 4, 6, 7};
